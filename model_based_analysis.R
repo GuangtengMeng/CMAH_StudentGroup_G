234ab7285ffa7323de3129e3057046fda830c163
druimeng <36269646+druimeng@users.noreply.github.com>
2021-09-17 21:19:30 +0800

    update for cognitive control hack

--- model_based_analysis.R
+++ model_based_analysis.R
@@ -68,257 +68,39 @@ tail(data_all)
 
 # filter
 ## check numbers of object conditions
-data_all %>% filter(!is.na(intensity) | !is.na(proportion)) %>% 
-  count(object)
-
-## output
 dataset <- data_all %>% 
-  select(participant, gender, object, intensity, proportion, 
-                               sliderSelfBS.response, krSelfBS.rt, sliderOtherBS.response, krOtherBS.rt,
-                               krAccept.keys, krAccept.corr, krAccept.rt, shareshock_target,
-                               sliderAS.response, krAS.rt,  ll, ul) %>% 
-  filter(!is.na(intensity) | !is.na(intensity))
-
-# Right choice & rating difference -------------------------------------------------------
-dataset$choiceRight <- ifelse(dataset$krAccept.keys == "right", 1, 0)
-dataset$shockTarget <- ifelse(dataset$krAccept.corr == 1 & dataset$shareshock_target == 0, "self", dataset$object)
-dataset$ratingDiff <- dataset$sliderOtherBS.response -dataset$sliderSelfBS.response
-
-## save .csv file named by participants
-write.csv(dataset, file = "data/01_dataset_001_002.csv")
-
-
-# read file ---------------------------------------------------------------
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-dataset <- read.csv("data/01_dataset_001_002.csv")
-
-N <- length(unique(dataset$participant))
-
-sum_dataset <- dataset %>%
-  group_by(participant, object) %>% 
-  summarise(acceptance = mean(krAccept.corr))
-
-sum_dataset <- merge(sum_dataset,
-                     sum_dataset %>% 
-                       group_by(object) %>% 
-                       summarise(mean_acceptance = mean(acceptance),
-                                 sd_acceptance = sd(acceptance)),
-                     by = "object")
-
-sum_dataset %>% 
-  ggplot(aes(x = object, y = acceptance, color = object, fill = object)) +
-  # display the mean accuracy in each condition with a bar
-  stat_summary(fun = mean, geom = "bar", width = .3, alpha = .3) +
-  # also display individual datapoints
-  geom_jitter(width = .2, alpha = .8) +
-  # make errorbars with confidence intervals
-  geom_errorbar(aes(ymin = mean_acceptance - (sd_acceptance / sqrt(N)),
-                    ymax = mean_acceptance + (sd_acceptance / sqrt(N))),
-                width = .15, position = position_dodge(.9)) + 
-  # hide the legend
-  guides(color = "none", fill = "none") +
-  labs(x = "Object", y = "Acceptance of Share") +
-  theme_minimal()
-ggsave("plots/acceptance.png", width = 6, height = 4)
-
-df_anova <- list(dataset$sliderSelfBS.response[dataset$shockTarget == "self"],
-             array_2 = dataset$sliderAS.response[dataset$shockTarget == "self"],
-             array_3 = dataset$sliderOtherBS.response[dataset$shockTarget != "self"],
-             array_4 = dataset$sliderAS.response[dataset$shockTarget != "self"])
-
-# Fit a logit model -------------------------------------------------------
-p_right <- function(ratingDiff, rightBias = 1, choiceSensitivity = 1) {
-  return(1 / (1 + exp(-rightBias - choiceSensitivity * ratingDiff)))
-}
-
-choiceModel1 <- function(ratingDiff, choiceRight, rightBias = 1, choiceSensitivity = 1){
-  p_right <- 1./(1 + exp(-rightBias - choiceSensitivity * ratingDiff))
-  #minimize the summed negative log-likelihood
-  likelihood <- p_right * (choiceRight == 1) + (1-p_right) * (choiceRight == 0) #likelihood of observed behavior given the model
-  negLogLikelihood <- -sum(log(likelihood)) #the negative log-likelihood is the target of the minimization algorithm
+  mutate(RTs = RT / 1000) %>% 
+  filter(!is.na(RT) & RTs>=0.150) %>% 
+  filter(Cond == 'SSI' | Cond == 'SSC' | Cond == 'SRI' | Cond == 'SRC') %>% 
+  mutate(boundary = ifelse(ACC == 1, "upper", "lower"))
   
-  return(negLogLikelihood)
-}
 
 
-bias <- seq(-2, 2, .1)
-choiceSensitivity <- seq(-2, 2, .1)
-subjects <- unique(dataset$participant)
-best_bias <- rep(NA, length(subjects))
-best_sensitivity <- rep(NA, length(subjects))
-best_nLL <- rep(1/0, length(subjects))
 
-bestParamsGridSearch <- matrix(data = 1/0, nrow = length(subjects), ncol = 3)
-
-
-# fit the model separately to all subjects by trying different values for 
-# both parameters
-for (s_idx in 1:length(subjects)) {
-  subj_tmp <- subjects[s_idx]
-  
-  for (bias_idx in 1:length(bias)) {
-    bias_tmp <- bias[bias_idx]
-    
-    for (choice_sens_idx in 1:length(choiceSensitivity)) {
-      choice_sens_tmp <- choiceSensitivity[choice_sens_idx]
-      
-      # feed the data irrespective of conditions
-      choiceRight <- dataset$choiceRight[dataset$participant == subj_tmp]
-      ratingDiff <- dataset$ratingDiff[dataset$participant == subj_tmp]
-      
-      # compute the negative log likelihood of the parameter values in the current
-      # iteration for subj_tmp
-      neg_log_lik <- choiceModel1(ratingDiff = ratingDiff, choiceRight = choiceRight,
-                                  rightBias = bias_tmp, choiceSensitivity = choice_sens_tmp)
-      
-      # update the best parameter values if the values return a smaller (i.e., better)
-      # negative log likelihood
-      if (neg_log_lik < bestParamsGridSearch[s_idx, 3]) {
-        bestParamsGridSearch[s_idx, 1] <- bias_tmp
-        bestParamsGridSearch[s_idx, 2] <- choice_sens_tmp
-        bestParamsGridSearch[s_idx, 3] <- neg_log_lik
-      }
-    }
-  }
-}
-
-#### Fit the model using an optimization algorithm (i.e., Nelder-Mead optimization)
-# nmk_results <- vector("list", length = N)
-bestParamsnmkSearch <- matrix(data = 1/0, nrow = length(subjects), ncol = 3)
-
-for (s_idx in 1:length(subjects)) {
-  subj_tmp <- subjects[s_idx]
-  
-  # starting value for two parameters
-  startVal <- c(bestParamsGridSearch[s_idx, 1], bestParamsGridSearch[s_idx, 2])
-  
-  # feed data
-  choiceRight <- dataset$choiceRight[dataset$participant == subj_tmp]
-  ratingDiff <- dataset$ratingDiff[dataset$participant == subj_tmp]
-  
-  # use nmk function of R-package dfoptim
-  fitModel <- nmk(par = startVal, fn = function(par) {
-    choiceModel1(rightBias = par[1], choiceSensitivity = par[2],
-                 choiceRight = choiceRight, ratingDiff = ratingDiff)
-  })
-  
-  # save outputs and print convergence messages
-  bestParamsnmkSearch[s_idx, 1] <- fitModel$par[1]
-  bestParamsnmkSearch[s_idx, 2] <- fitModel$par[2]
-  bestParamsnmkSearch[s_idx, 3] <- fitModel$value
-  
-  print(fitModel$message)
-}
-
-
-# rightBiase
-t.test(bestParamsnmkSearch[,1] - 0)
-
-# choiceSensitivity
-t.test(bestParamsnmkSearch[,2] - 0)
-
-
-choiceModel2 <- function(ratingDiff, choiceRight, recall_condition, 
-                         rightBias = 1, choiceSensitivity = c(1, 1)) {
-  choiceSensitivity_vector <- choiceSensitivity[1] * (object=="partner")+
-    choiceSensitivity[2] * (recallType=="stranger")
-  
-  p_right <- 1 / (1 + exp(-rightBias - choiceSensitivity_vector * ratingDiff))
-  neg_log_lik <- - (sum(log(p_right[choiceRight == 1])) + 
-                      sum(log(1 - p_right[choiceRight == 0])))
-  return(neg_log_lik)
-}
-
-
-#### Assume that the choiceSensitivity parameter differs across conditions ####
-bestParamsnmkSearch_conditions <- matrix(data = 1/0, nrow = length(subjects), ncol = 4)
-
-
-for (s_idx in 1:length(subjects)) {
-  subj_tmp <- subjects[s_idx]
-  
-  # starting value for two parameters, vector for sensitivity
-  startVal <- c(bestParamsGridSearch[s_idx, 1], rep(bestParamsGridSearch[s_idx, 2],3))
-  
-  # feed data
-  choiceRight <- dataset$choiceRight[dataset$subjID == subj_tmp]
-  valDiff <- dataset$valDiff[dataset$subjID == subj_tmp]
-  recallType <- dataset$recallType[dataset$subjID == subj_tmp]
-  
-  # use nmk function of R-package dfoptim
-  fitModel <- nmk(par = startVal, fn = function(par) 
-    choiceModel2(rightBias = par[1], choiceSensitivity = par[2:4],
-                 choiceRight = choiceRight, valDiff = valDiff, recall_condition = recallType))
-  
-  # save outputs and print convergence messages
-  for (i in 1:4) {
-    bestParamsnmkSearch_conditions[s_idx, i] <- fitModel$par[i]
-  }
-  bestParamsnmkSearch_conditions[s_idx, i+1] <- fitModel$value
-  
-  print(fitModel$message)
-}
-
-#### Task 3.3
-## compare the parameters across conditions with plot
-df_data_plot <- data.frame(recallType = c(rep(0,90), rep(1,90), rep(2,90)),
-                           choiceSensitivity = c(bestParamsnmkSearch_conditions[,2:4]))
-
-plot(x = df_data_plot$recallType, y = df_data_plot$choiceSensitivity, ylim = c(-0.2,1.0))
-
-ggbetweenstats(df_data_plot, x = recallType, y = choiceSensitivity)
-
-#### Have a look at the parameter estimates
-df_data_plot <- data.frame(object = c("partner", "stranger"),
-                           choiceSensitivity = c(bestParamsnmkSearch[,1:2]))
-
-plot(x = df_data_plot$object, y = df_data_plot$choiceSensitivity, ylim = c(-0.2,1.0))
-
-df_data_plot <- merge(df_data_plot,
-                      df_data_plot %>% 
-                        group_by(object) %>% 
-                        summarise(mean_choiceSensitivity = mean(choiceSensitivity),
-                                  sd_choiceSensitivity = sd(choiceSensitivity)),
-                      by = "object")
-
-df_data_plot %>% 
-  ggplot(aes(x = object, y = choiceSensitivity, color = object, fill = object)) +
-  # display the mean accuracy in each condition with a bar
-  stat_summary(fun = mean, geom = "bar", width = .3, alpha = .3) +
-  # also display individual datapoints
-  geom_jitter(width = .2, alpha = .8) +
-  # make errorbars with confidence intervals
-  geom_errorbar(aes(ymin = mean_choiceSensitivity - (sd_choiceSensitivity / sqrt(N)),
-                    ymax = mean_choiceSensitivity + (sd_choiceSensitivity / sqrt(N))),
-                width = .15, position = position_dodge(.9)) + 
-  # hide the legend
-  guides(color = "none", fill = "none") +
-  labs(x = "Object", y = "Choice Sensitivity based on logit model") +
-  theme_minimal()
-
+# DDM ---------------------------------------------------------------------
 #### Fit a Diffusion Model ####
 # Have a look at the RT data / RT histograms
-par(fig=c(0,1,0.3,1))
-hist(dataset$krAccept.rt[(dataset$object=="partner")&(dataset$krAccept.corr==1)],breaks = (0:50)/10,col = 'red',xlab = '',main = '',xaxt='n')
-hist(dataset$krAccept.rt[(dataset$object=="partner")&(dataset$krAccept.corr==0)],breaks = (0:50)/10,col = 'darkred',add = TRUE)
-legend(2.5,250,c('partner - accept','partner - reject'),fill=c('red','darkred'),bty='n')
-title('RT histograms per condition')
-# par(fig=c(0,1,0.25,0.75),new=TRUE)
-# hist(dataset$RT[(dataset$recallType==1)&(dataset$accuracy==1)],breaks = (0:50)/10,col = 'green',xlab = '',main = '',xaxt='n')
-# hist(dataset$RT[(dataset$recallType==1)&(dataset$accuracy==0)],breaks = (0:50)/10,col = 'darkgreen',add = TRUE)
-# legend(2.5,400,c('one remembered - correct','one remembered - error'),fill=c('green','darkgreen'),bty='n')
-par(fig=c(0,1,0,0.7),new=TRUE)
-hist(dataset$krAccept.rt[(dataset$object=="stranger")&(dataset$krAccept.corr==1)],breaks = (0:50)/10,col = 'blue',xlab = 'RT in sec',main = '')
-hist(dataset$krAccept.rt[(dataset$object=="stranger")&(dataset$krAccept.corr==0)],breaks = (0:50)/10,col = 'darkblue',add = TRUE)
-legend(2.5,275,c('both remembered - correct','both remembered - error'),fill=c('blue','darkblue'),bty='n')
+par(mfrow=c(2,2))
+hist(dataset$RTs[(dataset$Cond=="SSI")&(dataset$ACC==1)],breaks = (0:60)/40,col = 'red',xlab = '',main = '',xaxt='n')
+hist(dataset$RTs[(dataset$Cond=="SSI")&(dataset$ACC==0)],breaks = (0:60)/40,col = 'darkred',add = TRUE)
+legend(2.5,250,c('SSI - Correct','SSI - Incorrect'),fill=c('red','darkred'),bty='n')
 
 
-# prepare the data
-ddm_dataset <- dataset %>% 
-  # filter trials where the value difference is zero (since they can't be
-  # allocated to either boundary)
-  filter(!is.na(krAccept.rt)) %>% 
-  mutate(boundary = ifelse(correct == 1, "upper", "lower")) -> ddm_dataset
+hist(dataset$RTs[(dataset$Cond=="SSC")&(dataset$ACC==1)],breaks = (0:60)/40,col = 'blue',xlab = '',main = '',xaxt='n')
+hist(dataset$RTs[(dataset$Cond=="SSC")&(dataset$ACC==0)],breaks = (0:60)/40,col = 'darkblue',add = TRUE)
+legend(2.5,250,c('SSC - Correct','SSC - Incorrect'),fill=c('blue','darkblue'),bty='n')
+
+hist(dataset$RTs[(dataset$Cond=="SRI")&(dataset$ACC==1)],breaks = (0:60)/40,col = 'green',xlab = '',main = '',xaxt='n')
+hist(dataset$RTs[(dataset$Cond=="SRI")&(dataset$ACC==0)],breaks = (0:60)/40,col = 'darkgreen',add = TRUE)
+legend(2.5,250,c('SRI - Correct','SRI - Incorrect'),fill=c('green','darkgreen'),bty='n')
+
+
+hist(dataset$RTs[(dataset$Cond=="SRC")&(dataset$ACC==1)],breaks = (0:60)/40,col = 'cyan',xlab = '',main = '',xaxt='n')
+hist(dataset$RTs[(dataset$Cond=="SRC")&(dataset$ACC==0)],breaks = (0:60)/40,col = 'darkcyan',add = TRUE)
+legend(2.5,250,c('SRC - Correct','SRC - Incorrect'),fill=c('cyan','darkcyan'),bty='n')
+
+mtext("RT histograms per condition",side=3,line=-22,outer=TRUE)
+ggsave("RT histograms per condition.png", width = 6, height = 4)
 
 ## returns the deviance of the four-parameter diffusion model 
 ## for given decisions (boundary), response times (RTs) and diffusion model
@@ -343,20 +125,20 @@ diffusion_deviance(ddm_dataset$krAccept.rt, ddm_dataset$boundary, 0.3, 1, 0.5, 0
 
 # initialize the df to store results
 results_dm <- data.frame(matrix(nrow = 0, ncol = 7))
-names(results_dm) <- c("participant", "object", "drift_rate", "threshold", 
+names(results_dm) <- c("Subject", "Cond", "drift_rate", "threshold", 
                        "starting_point", "non_decision_time", "deviance")
-subjects <- unique(dataset$participant)
-conditions <- unique(dataset$object)
+subjects <- unique(dataset$Subject)
+conditions <- unique(dataset$Cond)
 
 # loop through subjects + recall conditions
 for (subj_tmp in 1:length(subjects)) {
   for (cond_tmp in 1:length(conditions)) {
     # filter the relevant data for the respective subject: the decisions
     # ("upper" for correct, "lower" for incorrect decisions) and response times
-    boundaries <- ddm_dataset$boundary[ddm_dataset$participant == subjects[subj_tmp] &
-                                         ddm_dataset$object == conditions[cond_tmp]]
-    RTs <- ddm_dataset$krAccept.rt[ddm_dataset$participant == subjects[subj_tmp] &
-                                     ddm_dataset$object == conditions[cond_tmp]]
+    boundaries <- dataset$boundary[dataset$Subject == subjects[subj_tmp] &
+                                     dataset$Cond == conditions[cond_tmp]]
+    RTs <- dataset$RTs[dataset$Subject == subjects[subj_tmp] &
+                         dataset$Cond == conditions[cond_tmp]]
     
     # fit the model using the Nelder-Mead algorithm
     fitModel <- nmk(par = start_val, fn = function(par) {
@@ -395,10 +177,25 @@ for (n_plot in 1:length(plot_list)) {
 }
 
 
+plot_dr <- results_dm %>% 
+  filter(Subject != 115) %>% 
+  
+
+ggbetweenstats(plot_dr, x = Cond, y = drift_rate)
+ggsave("drift rate.png", width = 6, height = 4)
+ggbetweenstats(plot_dr, x = Cond, y = starting_point)
+ggsave("starting_point.png", width = 6, height = 4)
+ggbetweenstats(plot_dr, x = Cond, y = threshold)
+ggsave("threshold.png", width = 6, height = 4)
+ggbetweenstats(plot_dr, x = Cond, y = non_decision_time)
+ggsave("non_decision_time.png", width = 6, height = 4)
+
+
+
 # drift rate
 results_dm %>%
-  ggplot(aes(x = object, y = drift_rate, group = object, 
-             color = object, fill = object)) +
+  ggplot(aes(x = Cond, y = drift_rate, group = Cond, 
+             color = Cond, fill = Cond)) +
   geom_jitter(alpha = .6, width = .13) + 
   geom_boxplot(width = .4, alpha = .5) + 
   guides(fill = "none", color = "none") +
@@ -406,6 +203,12 @@ results_dm %>%
   labs(y = "Drift Rate (v)", x = "Object")
 
 ggsave("plots/drift rate.png", width = 6, height = 4)
+ggbetweenstats(results_dm, x = Cond, y = drift_rate)
+results_drift <- aov_ez(id = "Subject",
+                        dv = "drift_rate",
+                        data = results_dm,
+                        within = c("Cond"),
+                        anova_table = list(es = "pes"))
 # descriptive difference in drift rate
 
 # starting point
@@ -445,10 +248,10 @@ results_dm %>%
 ggsave("plots/non_decision_time.png", width = 6, height = 4)
 
 
-results_drift <- aov_ez(id = "participant",
+results_drift <- aov_ez(id = "Subject",
                         dv = "drift_rate",
                         data = results_dm,
-                        within = c("object"),
+                        within = c("Cond"),
                         anova_table = list(es = "pes"))
 # p < .001 (very significant^^)
 
